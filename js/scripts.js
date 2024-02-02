621c32c88af23fe7a5d5a1bea609d90240c9282c
lidakisel <554vel@ukr.net>
2024-02-02 17:35:23 +0200

    carousel

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:5
+        }
+    }
+});
